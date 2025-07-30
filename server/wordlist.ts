16e067a2cf6da23e475e94d1ff3c8cb9fd09ac03
Shek Terry <terry_shekhinshing@msn.com>
2025-07-30 23:28:11 +0800

    correct word list

--- server/wordlist.ts
+++ server/wordlist.ts
@@ -1,2 +1,12 @@
-export const wordList = ["apple", "grape", "lemon", "peach", "mango", "melon"];
+export const wordList = [
+  "hello",
+  "world",
+  "quite",
+  "fancy",
+  "fresh",
+  "panic",
+  "crazy",
+  "buggy",
+  "scare",
+];
 // This is a sample word list. You can replace it with a larger list or fetch from an external source.
